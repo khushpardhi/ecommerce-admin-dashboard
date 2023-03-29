85eaffa0b7409cff5eac253a0b726730206f4dd0
khushpardhi <pardhikhush@gmail.com>
2023-03-29 15:16:44 +0530

    dashboard done

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login  from './pages/Login';
+import Resetpassword from './pages/Resetpassword';
+import Forgotpassword from './pages/Forgotpassword';
+import MainLayout from './components/MainLayout';
+import Enquiries from './pages/Enquiries';
+import  Bloglist  from "./pages/Bloglist";
+import Blogcatlist from "./pages/Blogcatlist";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+
+import Colorlist from "./pages/Colorlist";
+import Addcolor from "./pages/Addcolor";
+import Categorylist from "./pages/Categorylist";
+import Brandlist from "./pages/Brandlist";
+import Productlist from "./pages/Productlist";
+import Addblog from "./pages/Addblog";
+import Addproduct from "./pages/Addproduct";
+import Addcat from './pages/Addcat';
+import Addblogcat from './pages/Addblogcat';
+import AddBrand from './pages/Addbrand';
+
+
+
+function App() {    
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/reset-password" element={<Resetpassword/>}/>
+        <Route path="/forgot-password" element={<Forgotpassword/>}/>
+        <Route path="/admin" element={<MainLayout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path="enquiries" element={<Enquiries/>} />
+          <Route path="blog-list" element={<Bloglist />} />
+          <Route path="blog-category-list" element={<Blogcatlist />} />
+          <Route path="blog-category" element={<Addblogcat />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="list-color" element={<Colorlist />} />
+          <Route path="color" element={<Addcolor />} />
+          <Route path="list-category" element={<Categorylist />} />
+          <Route path="category" element={<Addcat />} />
+          <Route path="list-brand" element={<Brandlist/>} />
+          <Route path="brand" element={<AddBrand/>} />
+          <Route path="list-product" element={<Productlist/>} />
+          <Route path="product" element={<Addproduct/>} />
+          <Route path="blog" element={<Addblog/>} />
+
+        </Route>
+
+           
+        </Routes>
+    </Router >
+  );
+}
+export default App;
